ad546050373021dd140bee14c2aa05499570e34e
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2016-11-28 09:18:28 -0800

    minor fix

--- MovieLensALS.py
+++ MovieLensALS.py
@@ -68,97 +68,7 @@ def computeRmse(model, data, n):
       .values()
     return sqrt(predictionsAndRatings.map(lambda x: (x[0] - x[1]) ** 2).reduce(add) / float(n))
 
-def create_data_sets(sc, myRatingsRDD):
-    """
-    DEPRACATED (no longer used)
-    """
-    # load ratings and movie titles
 
-    movieLensHomeDir = sys.argv[1]
-
-    # ratings is an RDD of (last digit of timestamp, (userId, movieId, rating))
-    ratings = sc.textFile(join(movieLensHomeDir, "ratings.dat")).map(parseRating)
-
-    # movies is an RDD of (movieId, movieTitle)
-    movies = dict(sc.textFile(join(movieLensHomeDir, "movies.dat")).map(parseMovie).collect())
-
-    numRatings = ratings.count()
-    numUsers = ratings.values().map(lambda r: r[0]).distinct().count()
-    numMovies = ratings.values().map(lambda r: r[1]).distinct().count()
-
-    print "Got %d ratings from %d users on %d movies." % (numRatings, numUsers, numMovies)
-
-    # split ratings into train (60%), validation (20%), and test (20%) based on the 
-    # last digit of the timestamp, add myRatings to train, and cache them
-
-    # training, validation, test are all RDDs of (userId, movieId, rating)
-
-    training = ratings.filter(lambda x: x[0] < 6) \
-      .values() \
-      .union(myRatingsRDD) \
-      .repartition(numPartitions) \
-      .cache()
-
-    validation = ratings.filter(lambda x: x[0] >= 6 and x[0] < 8) \
-      .values() \
-      .repartition(numPartitions) \
-      .cache()
-
-    test = ratings.filter(lambda x: x[0] >= 8).values().cache()
-
-
-
-    numTraining = training.count()
-    numValidation = validation.count()
-    numTest = test.count()
-
-    print "Training: %d, validation: %d, test: %d" % (numTraining, numValidation, numTest)
-
-    return training, test, validation, movies
-
-
-def create_best_model(sc, training, test, validation):
-    """
-    DEPRACATED (no longer used)
-    """
-    numTraining = training.count()
-    numValidation = validation.count()
-    numTest = test.count()
-
-    ranks = [8, 12]
-    lambdas = [0.1, 10.0]
-    numIters = [10, 20]
-    bestModel = None
-    bestValidationRmse = float("inf")
-    bestRank = 0
-    bestLambda = -1.0
-    bestNumIter = -1
-
-    for rank, lmbda, numIter in itertools.product(ranks, lambdas, numIters):
-        model = ALS.train(training, rank, numIter, lmbda, seed=7)
-        validationRmse = computeRmse(model, validation, numValidation)
-        print "RMSE (validation) = %f for the model trained with " % validationRmse + \
-              "rank = %d, lambda = %.1f, and numIter = %d." % (rank, lmbda, numIter)
-        if (validationRmse < bestValidationRmse):
-            bestModel = model
-            bestValidationRmse = validationRmse
-            bestRank = rank
-            bestLambda = lmbda
-            bestNumIter = numIter
-
-    testRmse = computeRmse(bestModel, test, numTest)
-
-    # evaluate the best model on the test set
-    print "The best model was trained with rank = %d and lambda = %.1f, " % (bestRank, bestLambda) \
-      + "and numIter = %d, and its RMSE on the test set is %f." % (bestNumIter, testRmse)
-
-    # compare the best model with a naive baseline that always returns the mean rating
-    meanRating = training.union(validation).map(lambda x: x[2]).mean()
-    baselineRmse = sqrt(test.map(lambda x: (meanRating - x[2]) ** 2).reduce(add) / numTest)
-    improvement = (baselineRmse - testRmse) / baselineRmse * 100
-    print "The best model improves the baseline by %.2f" % (improvement) + "%."
-
-    return bestModel
 
 def build_recommendations(sc, myRatings, model):
     """
@@ -447,6 +357,7 @@ if __name__ == "__main__":
             type=str, help="File to write the output. " +\
                     "Output.txt by default")
 
+
     args = parser.parse_args()
     rank = args.rank
     lmbda = args.lmbda
@@ -467,7 +378,7 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf)
 
 ######################################## Fixes Stack Overflow issue when training ALS
-    sc.setCheckpointDir(sys.argv[2])
+    sc.setCheckpointDir(movieLensHomeDir)
     ALS.checkpointInterval = 2
 #######################################
 
