86dd96117b577b9ba43d63882371c5329a6dd6d7
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2017-03-30 19:49:56 -0700

    Minor fix

--- MovieLensALS.py
+++ MovieLensALS.py
@@ -1909,7 +1909,7 @@ if __name__ == "__main__":
 
     # create the initial training dataset with default ratings
     training = ratings\
-      .filter(lambda x: (x[2] in all_movies) and (True or x[0] < 3))\
+      .filter(lambda x: (x[1][1] in all_movies) and (True or x[0] < 3))\
       .values() \
       .repartition(numPartitions) \
       .cache()
