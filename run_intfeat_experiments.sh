c3edca7a3e27688c8aeebf6e8758a3143766f311
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2017-05-13 18:24:19 -0700

    experiments

--- run_intfeat_experiments.sh
+++ run_intfeat_experiments.sh
@@ -54,25 +54,38 @@
 #done
 #echo "Rank 3 linear regression done after $(($SECONDS - $_start)) seconds"
 
-echo `date`
-echo "Doing logistic regression rank 3"
-iteration=0
-_start=$SECONDS
-iteration_start=$SECONDS
-until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating tvtropes imdb_keywords imdb_producer imdb_director tags --cross-validation 70 --regression-model logistic --normalize --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_logistic_internal_rank_3.state  > logs/internal_regression_all_logistic.txt
-do
-    echo "Iteration $iteration of logistic regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-    iteration_start=$SECONDS
-    iteration=$(($iteration + 1))
-done
-echo "Rank 3 logistic regression done after $(($SECONDS - $_start)) seconds"
+#echo `date`
+#echo "Doing logistic regression rank 3"
+#iteration=0
+#_start=$SECONDS
+#iteration_start=$SECONDS
+#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating tvtropes imdb_keywords imdb_producer imdb_director tags --cross-validation 70 --regression-model logistic --normalize --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_logistic_internal_rank_3.state  > logs/internal_regression_all_logistic.txt
+#do
+#    echo "Iteration $iteration of logistic regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
+#    iteration_start=$SECONDS
+#    iteration=$(($iteration + 1))
+#    sleep 10000000
+#done
+#echo "Rank 3 logistic regression done after $(($SECONDS - $_start)) seconds"
 
 echo `date`
 echo "Doing tree regression rank 12"
 iteration=0
 _start=$SECONDS
 iteration_start=$SECONDS
-until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --normalize  --persist-dir ~/all_tree_internal.state  > logs/internal_regression_all_tree.txt
+until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_tree_internal.state  > logs/internal_regression_all_tree.txt
+do
+    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
+    iteration_start=$SECONDS
+    iteration=$(($iteration + 1))
+done
+echo "Rank 12 tree regression done after $(($SECONDS - $_start)) seconds"
+echo `date`
+echo "Doing tree regression deep rank 12"
+iteration=0
+_start=$SECONDS
+iteration_start=$SECONDS
+until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 - --max-depth 8 --persist-dir ~/all_tree_internal_deep.state  > logs/internal_regression_all_tree_deep.txt
 do
     echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
     iteration_start=$SECONDS
