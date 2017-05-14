60ba5187eb999b6cadb513d9992154c1bf214cc5
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2017-05-14 11:14:53 -0700

    experiments

--- run_intfeat_experiments.sh
+++ run_intfeat_experiments.sh
@@ -68,24 +68,24 @@
 #done
 #echo "Rank 3 logistic regression done after $(($SECONDS - $_start)) seconds"
 
-echo `date`
-echo "Doing tree regression rank 12"
-iteration=0
-_start=$SECONDS
-iteration_start=$SECONDS
-until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_tree_internal.state  > logs/internal_regression_all_tree.txt
-do
-    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-    iteration_start=$SECONDS
-    iteration=$(($iteration + 1))
-done
-echo "Rank 12 tree regression done after $(($SECONDS - $_start)) seconds"
-echo `date`
+#echo `date`
+#echo "Doing tree regression rank 12"
+#iteration=0
+#_start=$SECONDS
+#iteration_start=$SECONDS
+#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_tree_internal.state  > logs/internal_regression_all_tree.txt
+#do
+#    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
+#    iteration_start=$SECONDS
+#    iteration=$(($iteration + 1))
+#done
+#echo "Rank 12 tree regression done after $(($SECONDS - $_start)) seconds"
+#echo `date`
 echo "Doing tree regression deep rank 12"
 iteration=0
 _start=$SECONDS
 iteration_start=$SECONDS
-until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 - --max-depth 8 --persist-dir ~/all_tree_internal_deep.state  > logs/internal_regression_all_tree_deep.txt
+until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50  --max-depth 8 --persist-dir ~/all_tree_internal_deep.state  > logs/internal_regression_all_tree_deep.txt
 do
     echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
     iteration_start=$SECONDS
