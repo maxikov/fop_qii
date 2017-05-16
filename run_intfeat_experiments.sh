e86ee86a1b577d50be44d09b2b3c78792d00e360
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2017-05-15 21:05:26 -0700

    Much better experimental script

--- run_intfeat_experiments.sh
+++ run_intfeat_experiments.sh
@@ -1,145 +1,132 @@
 #!/usr/bin/env bash
 
+#Spark parameters
+MEMORY="15g"
+STATE_TMP_DIR_ROOT="~"
+LOG_DIR="logs"
+LOCAL_THREADS="\"*\""
+NUM_PARTITIONS=7
 
-#echo `date`
-#echo "Doing tree regression rank 1"
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 1 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --normalize  --persist-dir ~/all_tree_internal_rank_1.state  > logs/internal_regression_all_tree_rank_1.txt
-#do
-#    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-#echo "Rank 1 tree regression done after $(($SECONDS - $_start)) seconds"
+#Data source paths
+DATA_PATH="datasets/ml-20m"
+MOVIES_FILE="datasets/ml-20m/ml-20m.imdb.medium.csv"
+TVTROPES_FILE="datasets/dbtropes/tropes.csv"
 
-#echo `date`
-#echo "Doing tree regression rank 3"
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --normalize  --persist-dir ~/all_tree_internal_rank_3.state  > logs/internal_regression_all_tree_rank_3.txt
-#do
-#    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-#echo "Rank 3 tree regression done after $(($SECONDS - $_start)) seconds"
+#Recommender parameters
+RANK=3
+LMBDA=0.01
+NUM_ITER=300
+NON_NEGATIVE="" #Must be empty or --non-negative
+
+#Regression parameters
+METADATA_SOURCES="years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes"
+CROSS_VALIDATION=70
+REGRESSION_MODEL="regression_tree"
+NBINS=32
+MAX_DEPTH=5
+DROP_RARE_FEATURES=250
+DROP_RARE_MOVIES=50
+NORMALIZE="" #Must be empty or --normalize
+
+function make_commands() {
+	LOG_STATE_NAME="product_regression_all_${REGRESSION_MODEL}_rank_${RANK}_depth_${MAX_DEPTH}"
+	PERSIST_DIR="${STATE_TMP_DIR_ROOT}/${LOG_STATE_NAME}.state"
+	LOG_FILE="${LOG_DIR}/${LOG_STATE_NAME}.txt"
+
+	SPARK_SUBMIT="spark-submit --dirver-memory $MEMORY"
+
+	CHECKPOINT_DIR="$STATE_TMP_DIR_ROOT/spark_dir"
+	TEMP_DIR="$STATE_TMP_DIR_ROOT/spark_dir"
+
+	ARGS="--spark-executor-memory $MEMORY --local-threads $LOCAL_THREADS --num-partitions $NUM_PARTITIONS"
+	ARGS="${ARGS} --checkpoint-dir $CHECKPOINT_DIR --temp-dir $TEMP_DIR --persist-dir $PERSIST_DIR"
+	ARGS="${ARGS} --data-path $DATA_PATH --movies-file $MOVIES_FILE --tvtropes-file $TVTROPES_FILE"
+	ARGS="${ARGS} --rank $RANK --lmbda $LMBDA --num-iter $NUM_ITER $NON_NEGATIVE"
+	ARGS="${ARGS} --predict-product-features --metadata-sources $METADATA_SOURCES"
+	ARGS="${ARGS} --drop-rare-features $DROP_RARE_FEATURES --drop-rare-movies $DROP_RARE_MOVIES"
+	ARGS="${ARGS} --cross-validation $CROSS_VALIDATION --regression-model $REGRESSION_MODEL --nbins $NBINS $NORMALIZE"
+
+	WHOLE_COMMAND="$SPARK_SUBMIT MovieLensALS.py $ARGS >> $LOG_FILE"
+}
+
+function run_until_succeeds() {
+	MY_NAME="$REGRESSION_MODEL rank $RANK depth $MAX_DEPTH"
+	echo `date` "Running $MY_NAME, writing to $LOG_FILE, saving to $PERSIST_DIR"
+	echo $WHOLE_COMMAND > $LOG_FILE
+	iteration=0
+	_start=$SECONDS
+	iteration_start=$SECONDS
+	until $WHOLE_COMMAND
+	do
+		echo `date` "Iteration $iteration of $MY_NAME failed acter $(($SECONDS - $iteration_start)) seconds ($(($SECONDS - $_start)) total), trying again"
+		iteration_start=$SECONDS
+		iteration=$(($iteration + 1))
+	done
+	echo `date` "$MY_NAME done after $(($SECONDS - $_start)) seconds"
+}
+
+function rank_1_experiments() {
+	local RANK=1
+	make_commands
+	mkdir -p $PERSIST_DIR
+	run_until_succeeds
+	REFERENCE_MODEL="${PERSIST_DIR}/als_model.pkl"
+
+	local MAX_DEPTH=8
+	make_commands
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
+
+	local NORMALIZE="--normalize"
+	local REGRESSION_MODEL="linear"
+	make_commands
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
+}
 
 
-#echo `date`
-#echo "Doing tree regression depth 8 rank 3"
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating tvtropes imdb_keywords imdb_producer imdb_director tags --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_tree_internal_rank_3_depth_8.state  > logs/internal_regression_all_tree_rank_3_depth_8.txt
-#do
-#    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-#echo "Rank 3 tree depth 8 regression done after $(($SECONDS - $_start)) seconds"
+function rank_3_experiments() {
+	local RANK=3
+	make_commands
+	mkdir -p $PERSIST_DIR
+	run_until_succeeds
+	REFERENCE_MODEL="${PERSIST_DIR}/als_model.pkl"
 
-#echo `date`
-#echo "Doing linear regression rank 3"
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating  --cross-validation 70 --regression-model linear --nbins 32 --drop-rare-features 500 --drop-rare-movies 2 --normalize --filter-data-set 1 --persist-dir ~/all_linear_internal_rank_3.state  > logs/internal_regression_all_linear.txt
-#do
-#    echo "Iteration $iteration of linear regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-#echo "Rank 3 linear regression done after $(($SECONDS - $_start)) seconds"
+	local MAX_DEPTH=8
+	make_commands
+	mkdir -p $PERSIST_DIR
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
 
-#echo `date`
-#echo "Doing logistic regression rank 3"
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating tvtropes imdb_keywords imdb_producer imdb_director tags --cross-validation 70 --regression-model logistic --normalize --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_logistic_internal_rank_3.state  > logs/internal_regression_all_logistic.txt
-#do
-#    echo "Iteration $iteration of logistic regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#    sleep 10000000
-#done
-#echo "Rank 3 logistic regression done after $(($SECONDS - $_start)) seconds"
+	local NORMALIZE="--normalize"
+	local REGRESSION_MODEL="linear"
+	make_commands
+	mkdir -p $PERSIST_DIR
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
+}
 
-#echo `date`
-#echo "Doing tree regression rank 12"
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_tree_internal.state  > logs/internal_regression_all_tree.txt
-#do
-#    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-#echo "Rank 12 tree regression done after $(($SECONDS - $_start)) seconds"
-#echo `date`
+function rank_12_experiments() {
+	local RANK=12
+	make_commands
+	mkdir -p $PERSIST_DIR
+	run_until_succeeds
+	REFERENCE_MODEL="${PERSIST_DIR}/als_model.pkl"
 
-#echo `date`
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 1 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model linear --normalize --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_linear_internal_rank_1_same_as_tree.state > logs/internal_regression_all_linear_rank_1_same_as_tree.txt
-#do
-#    echo "Iteration $iteration of linear regression failed after $(($SECONDS - $_start)) ( $(($SECONDS - $iteration_start)) ) total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
+	local MAX_DEPTH=8
+	make_commands
+	mkdir -p $PERSIST_DIR
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
 
-#echo "Linear regression rank 1 same as tree done after $(($SECONDS - $_start))"
+	local NORMALIZE="--normalize"
+	local REGRESSION_MODEL="linear"
+	make_commands
+	mkdir -p $PERSIST_DIR
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
+}
 
-#echo `date`
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 3 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model linear --normalize --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_linear_internal_rank_3_same_as_tree.state > logs/internal_regression_all_linear_rank_3_same_as_tree.txt
-#do
-#    echo "Iteration $iteration of linear regression failed after $(($SECONDS - $_start)) ( $(($SECONDS - $iteration_start)) ) total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-
-#echo "Linear regression rank 3 same as tree done after $(($SECONDS - $_start))"
-
-#echo `date`
-#iteration=0
-#_start=$SECONDS
-#iteration_start=$SECONDS
-#until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model linear --normalize --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_linear_internal_rank_12_same_as_tree.state > logs/internal_regression_all_linear_rank_12_same_as_tree.txt
-#do
-#    echo "Iteration $iteration of linear regression failed after $(($SECONDS - $_start)) ( $(($SECONDS - $iteration_start)) ) total, trying again"
-#    iteration_start=$SECONDS
-#    iteration=$(($iteration + 1))
-#done
-
-#echo "Linear regression rank 12 same as tree done after $(($SECONDS - $_start))"
-
-echo "Doing tree regression deep rank 12"
-iteration=0
-_start=$SECONDS
-iteration_start=$SECONDS
-until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 500 --drop-rare-movies 50  --max-depth 8 --persist-dir ~/all_tree_internal_deep.state  > logs/internal_regression_all_tree_deep.txt
-do
-    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) seconds total, trying again"
-    iteration_start=$SECONDS
-    iteration=$(($iteration + 1))
-done
-echo "Rank 12 tree regression done after $(($SECONDS - $_start)) seconds"
-iteration=0
-_start=$SECONDS
-iteration_start=$SECONDS
-until spark-submit --driver-memory 15g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 15g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-product-features --metadata-sources years genres average_rating imdb_keywords imdb_producer imdb_director tags tvtropes --cross-validation 70 --regression-model linear --normalize --drop-rare-features 500 --drop-rare-movies 50 --persist-dir ~/all_linear_internal.state > logs/internal_regression_all_linear.txt
-do
-    echo "Iteration $iteration of linear regression failed after $(($SECONDS - $_start)) ( $(($SECONDS - $iteration_start)) ) total, trying again"
-    iteration_start=$SECONDS
-    iteration=$(($iteration + 1))
-done
-
-echo "Linear regression done after $(($SECONDS - $_start))"
+rank_1_experiments
+rank_3_experiments
+rank_12_experiments
