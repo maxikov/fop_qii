5b166233b5a84a47a1c618f9bd7a0188298a7c7c
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2017-05-16 17:29:50 -0700

    bug fixes

--- run_metadata_experiments.sh
+++ run_metadata_experiments.sh
@@ -12,17 +12,17 @@
 
 #echo "Linear regression done after $(($SECONDS - $_start))"
 
-iteration=0
-_start=$SECONDS
-iteration_start=$SECONDS
-until spark-submit --driver-memory 25g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 25g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-metadata --metadata-sources years genres average_rating --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 100  --persist-dir ~/metadata_years_genres_tree.state  > logs/metadata_regression_years_genres_tree.txt
-do
-    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) total, trying again"
-    iteration_start=$SECONDS
-    iteration=$(($iteration + 1))
-done
+#iteration=0
+#_start=$SECONDS
+#iteration_start=$SECONDS
+#until spark-submit --driver-memory 25g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 25g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-metadata --metadata-sources years genres average_rating --cross-validation 70 --regression-model regression_tree --nbins 32 --drop-rare-features 100  --persist-dir ~/metadata_years_genres_tree.state  > logs/metadata_regression_years_genres_tree.txt
+#do
+#    echo "Iteration $iteration of tree regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) total, trying again"
+#    iteration_start=$SECONDS
+#    iteration=$(($iteration + 1))
+#done
 
-echo "Tree regression done after $(($SECONDS - $_start))"
+#echo "Tree regression done after $(($SECONDS - $_start))"
 
 cp -rv ~/metadata_years_genres_tree.state/als_model.pkl ~/metadata_years_genres_linear.state/
 
@@ -32,6 +32,7 @@ iteration_start=$SECONDS
 until spark-submit --driver-memory 25g MovieLensALS.py --checkpoint-dir /home/maxikov/spark_dir --temp-dir /home/maxikov/spark_dir --spark-executor-memory 25g --local-threads "*" --lmbda 0.02 --num-iter 300 --non-negative --data-path datasets/ml-20m/ --movies-file datasets/ml-20m/ml-20m.imdb.medium.csv --tvtropes-file datasets/dbtropes/tropes.csv --num-partitions 7 --rank 12 --predict-metadata --metadata-sources years genres average_rating --cross-validation 70 --regression-model linear --nbins 32 --drop-rare-features 100  --persist-dir ~/metadata_years_genres_linear.state  > logs/metadata_regression_years_genres_linear.txt
 do
     echo "Iteration $iteration of linear regression failed after $(($SECONDS - $_start)) ($(($SECONDS - $iteration_start)) total, trying again"
+    sleep 1
     iteration_start=$SECONDS
     iteration=$(($iteration + 1))
 done
