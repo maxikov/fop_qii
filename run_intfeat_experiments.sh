da492c1bbc8119380b6d28f2d1b8d920394a561a
Maxim Kovalev <maxim.kovalev@2007.auditory.ru>
2017-05-17 14:33:10 -0700

    improved experiments

--- run_intfeat_experiments.sh
+++ run_intfeat_experiments.sh
@@ -27,11 +27,19 @@ MAX_DEPTH=5
 DROP_RARE_FEATURES=250
 DROP_RARE_MOVIES=50
 NORMALIZE="" #Must be empty or --normalize
+FEATURE_TRIM_PERCENTILE=90
 
 function make_commands() {
-	LOG_STATE_NAME="product_regression_all_${REGRESSION_MODEL}_rank_${RANK}_depth_${MAX_DEPTH}"
+	LOG_STATE_NAME="product_regression_all_${REGRESSION_MODEL}_rank_${RANK}"
+	if [ "$REGRESSION_MODEL" == "regression_tree" ]
+	then
+		LOG_STATE_NAME="${LOG_STATE_NAME}_depth_${MAX_DEPTH}"
+	fi
+	if [ ${FEATURE_TRIM_PERCENTILE} -ne 0 ]
+	then
+		LOG_STATE_NAME="${LOG_STATE_NAME}_feature_trim_percentile_${FEATURE_TRIM_PERCENTILE}"
+	fi
 	PERSIST_DIR="${STATE_TMP_DIR_ROOT}/${LOG_STATE_NAME}.state"
-	LOG_FILE="${LOG_DIR}/${LOG_STATE_NAME}.txt"
 
 	SPARK_SUBMIT="spark-submit --driver-memory $MEMORY"
 
@@ -45,6 +53,7 @@ function make_commands() {
 	ARGS="${ARGS} --predict-product-features --metadata-sources $METADATA_SOURCES"
 	ARGS="${ARGS} --drop-rare-features $DROP_RARE_FEATURES --drop-rare-movies $DROP_RARE_MOVIES"
 	ARGS="${ARGS} --cross-validation $CROSS_VALIDATION --regression-model $REGRESSION_MODEL --nbins $NBINS --max-depth $MAX_DEPTH $NORMALIZE"
+	ARGS="${ARGS} --feature-trim-percentile $FEATURE_TRIM_PERCENTILE"
 
 	WHOLE_COMMAND="$SPARK_SUBMIT MovieLensALS.py $ARGS"
 }
@@ -52,95 +61,59 @@ function make_commands() {
 function run_until_succeeds() {
 	MY_NAME="$REGRESSION_MODEL rank $RANK depth $MAX_DEPTH"
 	echo `date` "Running $MY_NAME, writing to $LOG_FILE, saving to $PERSIST_DIR"
-	echo $WHOLE_COMMAND > $LOG_FILE
 	iteration=0
 	_start=$SECONDS
 	iteration_start=$SECONDS
+	LOG_FILE="${LOG_DIR}/${LOG_STATE_NAME}_attempt_${iteration}.txt"
+	echo $WHOLE_COMMAND > $LOG_FILE
 	until $WHOLE_COMMAND >> $LOG_FILE
 	do
 		echo `date` "Iteration $iteration of $MY_NAME failed acter $(($SECONDS - $iteration_start)) seconds ($(($SECONDS - $_start)) total), trying again"
 		iteration_start=$SECONDS
 		iteration=$(($iteration + 1))
+		LOG_FILE="${LOG_DIR}/${LOG_STATE_NAME}_attempt_${iteration}.txt"
+		echo $WHOLE_COMMAND > $LOG_FILE
 	done
 	echo `date` "$MY_NAME done after $(($SECONDS - $_start)) seconds"
 }
 
-function rank_1_experiments() {
-	local RANK=1
+function copy_and_run() {
+	make_commands
+	mkdir -p $PERSIST_DIR
+	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
+	run_until_succeeds
+}
+
+function run_and_save() {
 	make_commands
 	mkdir -p $PERSIST_DIR
 	run_until_succeeds
 	REFERENCE_MODEL="${PERSIST_DIR}/als_model.pkl"
+}
+
+function rank_n_experiments() {
+	run_and_save()
 
 	local MAX_DEPTH=8
-	make_commands
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
+	copy_and_run()
 
 	local MAX_DEPTH=4
 	local NBINS=16
-	make_commands
-	mkdir -p $PERSIST_DIR
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
+	copy_and_run()
 
 	local NORMALIZE="--normalize"
 	local REGRESSION_MODEL="linear"
-	make_commands
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
+	copy_and_run()
 }
 
+RANK=1
+FEATURE_TRIM_PERCENTILE=0
+rank_n_experiments()
+FEATURE_TRIM_PERCENTILE=90
+rank_n_experiments()
 
-function rank_3_experiments() {
-	local RANK=3
-	make_commands
-	mkdir -p $PERSIST_DIR
-	run_until_succeeds
-	local REFERENCE_MODEL="${PERSIST_DIR}/als_model.pkl"
-
-	local MAX_DEPTH=8
-	make_commands
-	mkdir -p $PERSIST_DIR
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
-
-	local MAX_DEPTH=4
-	local NBINS=16
-	make_commands
-	mkdir -p $PERSIST_DIR
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
-
-	local NORMALIZE="--normalize"
-	local REGRESSION_MODEL="linear"
-	make_commands
-	mkdir -p $PERSIST_DIR
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
-}
-
-function rank_12_experiments() {
-	local RANK=12
-	make_commands
-	mkdir -p $PERSIST_DIR
-	run_until_succeeds
-	local REFERENCE_MODEL="${PERSIST_DIR}/als_model.pkl"
-
-	local MAX_DEPTH=8
-	make_commands
-	mkdir -p $PERSIST_DIR
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
-
-	local NORMALIZE="--normalize"
-	local REGRESSION_MODEL="linear"
-	make_commands
-	mkdir -p $PERSIST_DIR
-	cp -rv $REFERENCE_MODEL "${PERSIST_DIR}/"
-	run_until_succeeds
-}
-
-rank_1_experiments
-rank_3_experiments
-#rank_12_experiments
+RANK=3
+FEATURE_TRIM_PERCENTILE=0
+rank_n_experiments()
+FEATURE_TRIM_PERCENTILE=90
+rank_n_experiments()
