73153078fbc6975d98fb42a2e1edcbbc18f6c0dc
Sophia Kovaleva <sophia.kovaleva@west.cmu.edu>
2017-07-11 08:57:41 -0700

    gap statistic

--- product_clustering.py
+++ product_clustering.py
@@ -84,9 +84,9 @@ class ClassifierWrapper(object):
             X = np.array(data.map(lambda x: x.features).collect())
             Y = np.array(data.map(lambda x: x.label).collect())
             self.rank = X.shape[1]
-            layers = (self.rank*2, self.rank*2)
+            layers = (min(self.rank, 500), min(self.rank, 500))
             self._model = MLPClassifier(hidden_layer_sizes=layers,
-                    max_iter=2000, activation="logistic", solver="lbfgs")
+                    max_iter=1000, activation="tanh", solver="lbfgs")
             self._model.fit(X, Y)
         return self
 
@@ -176,6 +176,125 @@ def test_train_split(data, test_ratio):
     return test, training
 
 
+#https://datasciencelab.wordpress.com/2013/12/27/finding-the-k-in-k-means-clustering/
+def clusterdist(fal, centroid, cluster_id, normalize=True):
+    filter_f = functools.partial(lambda clid, ((mid, ftrs), cls):
+            cls == clid, cluster_id)
+    fal = fal.filter(filter_f)
+    nk = fal.count()
+    map_f = functools.partial(lambda centr, ((mid, ftrs), cls):
+            dist(centr, ftrs), centroid)
+    dists = fal.map(map_f)
+    Dk = dists.reduce(lambda a, b: a+b)
+    if not normalize:
+        Dk *= 2.0 * nk
+    return Dk
+
+def compactness(fal, centroids, n_clusters, log=True):
+    Wk = 0.0
+    for cluster_id in xrange(n_clusters):
+        centroid = centroids[cluster_id]
+        Dk = clusterdist(fal, centroid, cluster_id, normalize=True)
+        Wk += Dk
+    if log:
+        Wk = np.log(Wk)
+    return Wk
+
+def bounding_box(fal):
+    reduce_f_min = lambda ((mid1, ftrs1), cls1), ((mid2, ftrs2), cls2):\
+        ((mid1, map(min, zip(ftrs1, ftrs2)) ), cls1)
+    reduce_f_max = lambda ((mid1, ftrs1), cls1), ((mid2, ftrs2), cls2):\
+        ((mid1, map(max, zip(ftrs1, ftrs2)) ), cls1)
+    mins = fal.reduce(reduce_f_min)[0][1]
+    maxs = fal.reduce(reduce_f_max)[0][1]
+    return mins, maxs
+
+def generate_data_set(sc, mins, maxs, N):
+    rank = len(mins)
+    res = None
+    for i in xrange(rank):
+        col = np.random.uniform(mins[i], maxs[i], N)
+        col = col.reshape((N, 1))
+        if res is None:
+            res = col
+        else:
+            res = np.hstack((res, col))
+    res_rdd = sc.parallelize([x for x in res])
+    return res_rdd
+
+def BWkb(sc, fal, B, n_clusters, cluster_model, cluster_args, cluster_kwargs):
+    N = fal.count()
+    print "Got", N, "data points, computing bounding box"
+    mins, maxs = bounding_box(fal)
+    print "Box:", mins, maxs
+    Wkbs = []
+    for b in xrange(B):
+        print "Generating data set", b, "out of", B
+        data_set = generate_data_set(sc, mins, maxs, N)
+        print "Done"
+        print "Running clustering, kwargs:", cluster_kwargs
+        kmeans_model = cluster_model(*cluster_args, **cluster_kwargs)
+        centroids = list(map(np.array, kmeans_model.clusterCenters))
+        print "Done"
+        print "Labeling clusters"
+        clusters = kmeans_model.predict(data_set).map(float)
+        product_features = data_set.zipWithIndex().map(lambda (a, b): (b, a))
+        features_and_labels = common_utils.safe_zip(product_features, clusters)
+        print "Computing compactness"
+        wk = compactness(features_and_labels, centroids, n_clusters)
+        print "Compactness:", wk
+        Wkbs.append(wk)
+    avg_comp = np.mean(Wkbs)
+    print "Average compactness:", avg_comp
+    comp_std = np.std(Wkbs)
+    print "Standard deviation:", comp_std
+    sk = np.sqrt(1+1.0/B) * comp_std
+    print "sk:", sk
+    return avg_comp, sk
+
+def gap(sc, B, data_set, product_features, n_clusters, cluster_model,
+        cluster_args, cluster_kwargs):
+    print "Running clustering for", n_clusters, "clusters, kwargs:", cluster_kwargs
+    kmeans_model = cluster_model(*cluster_args, **cluster_kwargs)
+    centroids = list(map(np.array, kmeans_model.clusterCenters))
+    print "Done"
+    print "Labeling clusters"
+    clusters = kmeans_model.predict(data_set).map(float)
+    features_and_labels = common_utils.safe_zip(product_features, clusters)
+    print "Computing compactness"
+    wk = compactness(features_and_labels, centroids, n_clusters)
+    print "Computing estimated compactness"
+    avg_comp, sk = BWkb(sc, features_and_labels, B, n_clusters, cluster_model, cluster_args,
+            cluster_kwargs)
+    Gap = avg_comp - wk
+    return Gap, sk
+
+def choose_optimal_k(sc, B, data_set, product_features, max_clusters, cluster_model,
+        cluster_args, cluster_kwargs):
+    res = []
+    for n_clusters in xrange(1, max_clusters+1):
+        print "Trying", n_clusters
+        cluster_args_ = list(cluster_args) + [n_clusters]
+        Gap, sk = gap(sc, B, data_set, product_features, n_clusters, cluster_model,
+                      cluster_args_, cluster_kwargs)
+        res.append((Gap, sk))
+        if n_clusters in [1,2]:
+            continue
+        else:
+            Gap_k, sk_k = res[-2]
+            kp1 = Gap - sk
+            stat = Gap_k - kp1
+            print "Gap_{}: {}, Gap_{} - s_{}: {}".format(n_clusters - 1, Gap_k,
+                    n_clusters, n_clusters, kp1)
+            print "Gap_{} - (Gap_{} - s_{}): {}".format(n_clusters - 1,
+                    n_clusters, n_clusters, stat)
+            if Gap_k >= kp1:
+                print "Found optimal k:", n_clusters - 1
+                return n_clusters - 1
+    print "No change in gap statistic found, assuming k = 1"
+    return 1
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--persist-dir", action="store", type=str, help=\
@@ -194,6 +313,9 @@ def main():
     parser.add_argument("--cluster-model", action="store", type=str,
             default="kmeans", choices=["kmeans", "gmm", "spectral",
                 "agglomerative", "birch"])
+    parser.add_argument("--max-clusters", action="store", type=int,
+            help="Override --n-clusters and use gap statistic to" +\
+                    " determine the optimal k, no more than this.")
     parser.add_argument("--cluster-sample-size", action="store", type=int,
             default=10, help="Number of movies to display in each cluster")
     parser.add_argument("--test-ratio", action="store", type=float,
@@ -234,8 +356,17 @@ def main():
 
     print "Training", args.cluster_model
     if args.cluster_model == "kmeans":
-        kmeans_model = KMeans.train(data_set, args.n_clusters, maxIterations=100,
-            initializationMode="random")
+        def cluster_model(*args, **kwargs):
+            return KMeans.train(*args, **kwargs)
+        cluster_args = (data_set,)
+        cluster_kwargs = {"maxIterations": 100, "initializationMode": "random"}
+        if args.max_clusters is not None:
+            B = 10
+            k = choose_optimal_k(sc, B, data_set, product_features, args.max_clusters, cluster_model,
+                                 cluster_args, cluster_kwargs)
+            args.n_clusters = k
+        cluster_args = list(cluster_args) + [args.n_clusters]
+        kmeans_model = cluster_model(*cluster_args, **cluster_kwargs)
         centroids = list(map(np.array, kmeans_model.clusterCenters))
     elif args.cluster_model == "gmm":
         kmeans_model = GaussianMixture.train(data_set, args.n_clusters)
@@ -278,6 +409,11 @@ def main():
         key_f = functools.partial(lambda centroid, ((mid, ftrs), cls):
             dist(ftrs, centroid), centroid)
         sample = cur_mvs.takeOrdered(args.cluster_sample_size, key=key_f)
+        key_f_least = functools.partial(lambda centroid, ((mid, ftrs), cls):
+            -dist(ftrs, centroid), centroid)
+        sample_least = cur_mvs.takeOrdered(args.cluster_sample_size,
+                key=key_f_least)
+        sample = sample + sample_least
         text_sample = []
         for (i, ((mid, ftrs), cls)) in enumerate(sample):
             text_sample.append("\t{}: (dist: {}) {} (mid: {})".format(i, dist(ftrs, centroid),
--- run_clustering_experiments.sh
+++ run_clustering_experiments.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-algos="kmeans spectral agglomerative"
-models="mlpc decision_tree"
+#algos="kmeans spectral agglomerative"
+algos="kmeans"
+#models="mlpc decision_tree"
+models="decision_tree"
 log_dir="new_experiments/logs"
+#n_or_max="--n-clusters 15"
+n_or_max="--max-clusters 30"
 
 for algo in $algos
 do
 	for model in $models
 	do
 		log_file="${log_dir}/product_clustering_real_data_${algo}_${model}.txt"
-		whole_command="spark-submit --driver-memory 10g product_clustering.py --persist-dir archived_states/product_regression_all_regression_tree_rank_12_depth_5.state  --n-clusters 15 --cluster-model ${algo} --model ${model} --movies-file datasets/ml-20m/movies.csv >> $log_file"
+		whole_command="spark-submit --driver-memory 10g product_clustering.py --persist-dir archived_states/product_regression_all_regression_tree_rank_12_depth_5.state ${n_or_max} --cluster-model ${algo} --model ${model} --movies-file datasets/ml-20m/movies.csv >> $log_file"
 		echo `date` "Running ${whole_command}, writing to ${log_file}"
 		echo `date` $whole_command > $log_file
 		eval $whole_command
 		log_file="${log_dir}/product_clustering_larger_synth_${algo}_${model}.txt"
-		whole_command="spark-submit --driver-memory 10g product_clustering.py --persist-dir new_experiments/product_regression_all_regression_tree_rank_12_depth_5_larger_synth.state/ --user-profiles new_experiments/synth_data_set/profiles.pkl --n-clusters 15 --cluster-model ${algo} --model ${model} --movies-file datasets/ml-20m/movies.csv >> $log_file"
+		whole_command="spark-submit --driver-memory 10g product_clustering.py --persist-dir new_experiments/product_regression_all_regression_tree_rank_12_depth_5_larger_synth.state/ --user-profiles new_experiments/synth_data_set/profiles.pkl ${n_or_max} --cluster-model ${algo} --model ${model} --movies-file datasets/ml-20m/movies.csv >> $log_file"
 		echo `date` "Running ${whole_command}, writing to ${log_file}"
 		echo `date` $whole_command > $log_file
 		eval $whole_command
